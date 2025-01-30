1ea362f61eac1ade074309cecce2ce17898599da
miomate <mrspec@outlook.com>
2025-01-30 21:23:01 +0100

    the db connection in .env file was still local url...

--- db/index.js
+++ db/index.js
@@ -1,26 +1,46 @@
 const mongoose = require("mongoose");
-
-// ℹ️ MongoDB URI setup (using environment variable for production or fallback to local)
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/restAPI";
 
 const withDB = async (serverListener) => {
   try {
-    const connection = await mongoose.connect(MONGO_URI);
+    const x = await mongoose.connect(MONGO_URI);
     console.log(
-      `Connected to Mongo! Database name: "${connection.connections[0].name}"`
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
     if (typeof serverListener === "function") {
       serverListener();
     }
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    console.error("Mongo URI:", MONGO_URI); // Log the URI to check if it's correct
-    process.exit(1); // Optional: exit the process if MongoDB connection fails
+    console.error("Error connecting to mongo: ", error);
   }
 };
 
 module.exports = withDB;
+
+// const mongoose = require("mongoose");
+
+// // ℹ️ MongoDB URI setup (using environment variable for production or fallback to local)
+// const MONGO_URI =
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/restAPI";
+
+// const withDB = async (serverListener) => {
+//   try {
+//     const connection = await mongoose.connect(MONGO_URI);
+//     console.log(
+//       `Connected to Mongo! Database name: "${connection.connections[0].name}"`
+//     );
+//     if (typeof serverListener === "function") {
+//       serverListener();
+//     }
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error.message);
+//     console.error("Mongo URI:", MONGO_URI); // Log the URI to check if it's correct
+//     process.exit(1); // Optional: exit the process if MongoDB connection fails
+//   }
+// };
+
+// module.exports = withDB;
 
 // // ℹ️ package responsible to make the connection with mongodb
 // // https://www.npmjs.com/package/mongoose
