9b9631e7d5a65ec8687b8dd9b51d9ff99f572f63
miomate <mrspec@outlook.com>
2025-01-30 21:00:19 +0100

    fixing adding skill

--- models/City.model.js
+++ models/City.model.js
@@ -1,22 +1,12 @@
-const { Schema, model } = require('mongoose')
+const mongoose = require("mongoose");
 
-// TODO: Please make sure you edit the city model to whatever makes sense in this case
-const citySchema = new Schema(
-  {
-    city: {
-      type: String,
-      required: true,
-      lowercase: true,
-      unique: true,
-      trim: true,
-    },
-    
+const citySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-)
+});
 
 const City = mongoose.model("City", citySchema);
 
