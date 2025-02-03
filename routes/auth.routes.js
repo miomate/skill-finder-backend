0db18d65737cf76b53f38b14d2cceaeb425a1971
miomate <mrspec@outlook.com>
2025-02-03 23:29:49 +0100

    fixing my attempt to fix something else

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,7 +16,9 @@ router.post("/signup", async (req, res, next) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return res.status(400).json({ message: "Username and password required" });
+      return res
+        .status(400)
+        .json({ message: "Username and password required" });
     }
 
     const salt = bcrypt.genSaltSync(13);
@@ -26,6 +28,12 @@ router.post("/signup", async (req, res, next) => {
 
     res.status(201).json(newUser);
   } catch (error) {
+    // If duplicate key error, send friendly message
+    if (error.code === 11000) {
+      return res
+        .status(400)
+        .json({ message: "Username already taken. Choose another one." });
+    }
     next(error);
   }
 });
